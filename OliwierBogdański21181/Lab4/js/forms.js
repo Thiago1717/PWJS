f37b0ab1ef40f4234d3604e7de82345f754ddfc9
Oliwier Bogdański <oliwier.bogd001@gmail.com>
2025-04-07 20:31:57 +0200

    update

--- OliwierBogdański21181/Lab4/js/forms.js
+++ OliwierBogdański21181/Lab4/js/forms.js
@@ -1,37 +1,60 @@
-function checkForm() {
-    var error = false;
-    var errorText = "";
+function showError(id, message) {
+    const errorElement = document.getElementById(id);
+    errorElement.innerHTML = message;
+    errorElement.className = "alert alert-danger";
+}
 
-    var contactName = document.getElementById("contactName");
-    var contactEmail = document.getElementById("contactEmail");
-    var message = document.getElementById("message");
+function hideError(id) {
+    const errorElement = document.getElementById(id);
+    errorElement.className = "hide";
+}
 
-    if (contactName.value.trim() === "") {
-        errorText += "imię\n";
-        error = true;
-    }
-
-    if (contactEmail.value.trim() === "") {
-        errorText += "email\n";
-        error = true;
-    } else {
-        var email = contactEmail.value.trim();
-        var regex = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+\.)+[a-zA-Z]{2,}$/;
-        if (!regex.test(email)) {
-            errorText += "błędny email\n";
-            error = true;
-        }
-    }
-
-    if (message.value.trim() === "") {
-        errorText += "wiadomość\n";
-        error = true;
-    }
-
-    if (!error) {
-        return true;
-    } else {
-        alert("Nie wypełniłeś następujących pól:\n" + errorText);
+function validateName() {
+    const name = document.getElementById("contactName");
+    if (name.value.trim() === "") {
+        showError("errorName", "Podanie imienia jest wymagane!");
+        name.classList.add("is-invalid");
         return false;
+    } else {
+        hideError("errorName");
+        name.classList.remove("is-invalid");
+        name.classList.add("is-valid");
+        return true;
+    }
+}
+
+function validateEmail() {
+    const email = document.getElementById("contactEmail");
+    const value = email.value.trim();
+    const regex = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+\.)+[a-zA-Z]{2,}$/;
+
+    if (value === "") {
+        showError("errorEmail", "Adres email jest wymagany.");
+        email.classList.add("is-invalid");
+        return false;
+    } else if (!regex.test(value)) {
+        showError("errorEmail", "Podano błędny format adresu email.");
+        email.classList.add("is-invalid");
+        return false;
+    } else {
+        hideError("errorEmail");
+        email.classList.remove("is-invalid");
+        email.classList.add("is-valid");
+        return true;
+    }
+}
+
+function validateMessage() {
+    const message = document.getElementById("message");
+    const value = message.value.trim();
+    if (value === "") {
+        showError("errorMessage", "Wiadomość jest wymagana.");
+        message.classList.add("is-invalid");
+        return false;
+    } else {
+        hideError("errorMessage");
+        message.classList.remove("is-invalid");
+        message.classList.add("is-valid");
+        return true;
     }
 }
