57540e35032abb3298a964fb3d9ac7fb0e7d53b5
Oliwier Bogdański <oliwier.bogd001@gmail.com>
2025-04-07 10:28:23 +0200

    Update

---
+++ OliwierBogdański21181/Lab4/js/forms.js
@@ -0,0 +1,31 @@
+function checkForm() {
+    var error = false;
+    var errorText = "";
+
+    var contactName = document.getElementById("contactName");
+    var contactEmail = document.getElementById("contactEmail");
+
+    if (contactName.value.trim() === "") {
+        errorText += "imię\n";
+        error = true;
+    }
+
+    if (contactEmail.value.trim() === "") {
+        errorText += "email\n";
+        error = true;
+    } else {
+        var email = contactEmail.value.trim();
+        var regex = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+\.)+[a-zA-Z]{2,}$/;
+        if (!regex.test(email)) {
+            errorText += "błędny email\n";
+            error = true;
+        }
+    }
+
+    if (!error) {
+        return true;
+    } else {
+        alert("Nie wypełniłeś następujących pól:\n" + errorText);
+        return false;
+    }
+}
